474e486adca76fa090e21b4d74c5fee4ae0d4d16
Valentin Scevola <valentinscevolaaste@gmail.com>
2025-12-28 01:05:19 -0300

    Models and Admin Models for App Catalog created and working

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, ProductLink
 
 # Register your models here.
+
+
+#-- Imagen --#
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    ordering = ("order",)
+
+
+#-- Link --#
+class ProductLinkInline(admin.TabularInline):
+    model = ProductLink
+    extra = 1
+    ordering = ("order",)
+
+
+#-- Producto --#
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "is_active")
+    list_filter = ("is_active", "category")
+    search_fields = ("name",)
+    exclude = ("slug",)
+
+    list_display = (
+        "name",
+        "category",
+        "price",
+        "is_active",
+    )
+    list_filter = (
+        "category",
+        "is_active",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
+    inlines = [
+        ProductImageInline,
+        ProductLinkInline,
+    ]
+    ordering = ("name",)
+
+
+#-- Categoría --#
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("slug",)
+    
+    list_display = (
+        "name",
+        "is_active",
+        "order",
+    )
+    list_filter = (
+        "is_active",
+    )
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+    ordering = ("order", "name")
