e64933f2c5ef22c08bb2966425ef2d491bfc787d
Valentin Scevola <valentinscevolaaste@gmail.com>
2025-12-28 00:15:05 -0300

    Apps core and accounts created

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'apps.catalog',
+    'apps.core',
+    'apps.accounts',
+    'apps.catalog',    
 ]
 
 MIDDLEWARE = [
