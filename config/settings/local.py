987e95aa13ec1f569777a0fea8b79974416d2b30
Valentin Scevola <valentinscevolaaste@gmail.com>
2026-02-12 09:26:17 -0300

    change: Maps URL (now labels 'generado automáticamente')

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,3 +4,11 @@ from .base import *
 
 # Desarrollo: subdominios con localhost (ej: dulcetinta.localhost:8000)
 ROOT_DOMAIN = config("ROOT_DOMAIN", default="localhost")
+
+#DB Development
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
